4e913cbc24fd018b7ef7d247968e94288d766fcf
GardLundh <gard.lundh@gmail.com>
2020-07-14 14:35:42 +0200

    Updates

---
+++ precache-manifest.13f373c73ce88083737ded043bf47fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6464d301da272f999ba793e49b249d",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "b3a99d001bcfe137a0a3",
+    "url": "/COVID-19-Tracker/static/css/main.8462d0eb.chunk.css"
+  },
+  {
+    "revision": "9862a16171ceab5456d0",
+    "url": "/COVID-19-Tracker/static/js/2.1a4d36f5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.1a4d36f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a99d001bcfe137a0a3",
+    "url": "/COVID-19-Tracker/static/js/main.5b760427.chunk.js"
+  },
+  {
+    "revision": "dfba1439f23362f8978e",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.7a9a9f9b.js"
+  }
+]);
